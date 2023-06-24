1c8898f7b062c9f0727f901f16c161e8b9b2ffd6
Victoria Wolf <victoriiwolf@yahoo/com>
2023-06-24 22:51:34 +0700

    Forth homework is almost done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
-function Student(name, gender, age) {
-  
+function Student(name, gender, age, marks) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
-}
+  if (this.hasOwnProperty('marks') && this.marks !== []) {
+    this.marks.push(...marks);
+      } else {
+        return 0;
+      }
+    }
 
 Student.prototype.getAverage = function () {
-  
+    if(this.hasOwnProperty('marks') === false || this.marks === []) {
+        return 0;
+    } else {
+        let length = this.marks.length;
+        let sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+      return sum / length;
+    };
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    this.excluded = reason;
+    delete this.subject;
+    delete this.marks;
 }
