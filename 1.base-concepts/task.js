a7161d6ffefc40418f93a8e8608980d8c05fa365
Victoria Wolf <victoriiwolf@yahoo/com>
2023-07-06 22:41:51 +0700

    Sixth homework still in process

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,19 +1,23 @@
 "use strict"
+
+/* Task 1 */
+
 function solveEquation(a, b, c) {
   let arr = [];
   let D = Math.pow(b, 2) - 4 * a * c;
-  if(D < 0) {
-   return arr; 
-  } else if(D === 0) {
-    let X = -b / (2 * a);
-    return arr = [X];
-  } else if(D > 0) {
-    let X1 = (-b + Math.sqrt(D)) / (2 * a);
-    let X2 = (-b - Math.sqrt(D)) / (2 * a);
-    return  arr = [X1, X2];
+  if (D < 0) {
+    return arr;
+  } else if (D === 0) {
+    let x = -b / (2 * a);
+    return [x];
+  } else if (D > 0) {
+    let x1 = (-b + Math.sqrt(D))/(2 * a);
+    let x2 = (-b - Math.sqrt(D)) / (2 * a);
+    return [x1, x2];
   }
-
 }
+
+/* Task 2 */
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   const P = (percent / 100) / 12;
--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,44 +1,57 @@
-﻿function parseCount(number) {
-    let parseValue = Number.parseFloat(number)
-        if(Number.isNaN(parseValue)) throw new Error ('Невалидное значение');
-        return parseValue
-        }
+﻿/* Task 1 */
 
-function validateCount(number) {
-    try {
-        return parseCount(number);
-    } catch (error) {
-        return error;
-    }
+function parseCount(number) {
+	let parseValue = Number.parseFloat(number);
+	if (Number.isNaN(parseValue)) {
+		throw new Error('Невалидное значение');
+	}
+	return parseValue;
+}
+
+const validateCount = number => {
+	try {
+		return parseCount(number);
+	} catch (error) {
+		return error;
+	}
 }
 
 
-class Triangle{
-    constructor(a, b, c) {
-        this.a = a;
-        this.b = b;
-        this.c = c
-    }
-    
-    if(a > (b + c) || b > (a + c) || c > (a + b)) {
-        throw new Error ('Треугольник с такими сторонами не существует')
-    }
+/* Task 2 */
 
-    get perimeter() {
-        return this.a + this.b + this.c;
-    }
+class Triangle {
+	constructor(a, b, c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
 
-    get area() {
-        const S = this.perimeter() / 2;
-        this._area = Number(Math.sqrt((s * (s - this.a) * (s - this.b) * (s - this.c)).toFixed(3)));
-        return this._area;
-    }
+		if (a > b + c || b > a + c || c > a + b) {
+			throw new Error('Треугольник с такими сторонами не существует')
+		}
+	}
+
+	get perimeter() {
+		return this.a + this.b + this.c;
+	}
+
+	get area() {
+		const s = this.perimeter / 2;
+		return Number(Math.sqrt((s * (s - this.a) * (s - this.b) * (s - this.c)).toFixed(3)));
+	}
 }
 
-function getTriangle(a, b, c) {
-    try {
-        return new Triangle;
-        } catch (error) {
-            return 
-    }
+const getTriangle = (a, b, c) => {
+	try {
+		let triangle = new Triangle(a, b, c);
+		return triangle;
+	} catch (error) {
+		return {
+			get perimeter() {
+				return 'Ошибка! Треугольник не существует';
+			},
+			get area() {
+				return 'Ошибка! Треугольник не существует';
+			}
+		}
+	}
 }
