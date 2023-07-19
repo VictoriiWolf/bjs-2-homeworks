9ee46abc975f629e4495108dd5c722b15cce9618
Victoria Wolf <victoriiwolf@yahoo/com>
2023-07-19 12:24:43 +0700

    Seventh homework is in process

---
+++ 7.async/task.js
@@ -0,0 +1,75 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.intervalId = null;
+    }
+
+    addClock(time, callback) {
+        if(time === undefined || callback === undefined) {
+            throw new Error('Отсутсвуют обязательные аргуенты');
+        } 
+        
+        const check = this.alarmCollection.find(item => item.time === time);
+        if (check) {
+            console.warn('Уже присутствует звонок на это же время')
+        }
+
+        this.alarmCollection.push({
+            callback: callback,
+            time: time,
+            canCall: true
+        });
+    }
+
+    removeClock(time) {
+        this.alarmCollection = this.alarmCollection.filter(item => {
+            return item.time !== time;
+        });
+    }
+
+    getCurrentFormattedTime() {
+        const today = new Date();
+        let hours = today.getHours();
+        let minutes = today.getMinutes();
+
+        if (hours < 10) {
+            hours = '0' + hours;
+        }
+        if (minutes < 10) {
+            minutes = '0' + minutes;
+        }
+        let currentTime = `${hours}:${minutes}`;
+        return currentTime;
+    }
+
+    start() {
+        if(this.intervalId !== undefined) {
+            return;
+        } else {
+            this.intervalId = this.intervalId(() => {
+                this.alarmCollection.forEach((item) => {
+                    if ((this.getCurrentFormattedTime() === item.time) && item.canCall === true) {
+                        item.canCall = false;
+                        item.callback();
+                    }
+                });
+            });
+        }
+    }
+
+    stop() {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    }
+
+    resetAllCalls() {
+        this.alarmCollection.forEach(item => {
+            item.canCall = true;
+        });
+    }
+
+    clearAlarms() {
+        this.stop();
+        this.alarmCollection = [];
+    }
+}
